1b14d9cfb33a8b1cfcc3d6efeccb353d8d1a7fda
Namju Kim <cmsong111@naver.com>
2022-12-05 01:15:50 +0900

    test

--- src/main/java/com/javadeveloperzone/Controller/DemoController.java
+++ src/main/java/com/javadeveloperzone/Controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.javadeveloperzone;
+package com.javadeveloperzone.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
